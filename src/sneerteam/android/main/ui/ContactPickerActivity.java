d88bdcf3213607c11a818ef7b35307298c2373dc
Rodrigo B. de Oliveira <rbo@acm.org>
2014-03-22 13:40:09 -0300

    return (fake) public_key and nickname on click

--- src/sneerteam/android/main/ui/ContactPickerActivity.java
+++ src/sneerteam/android/main/ui/ContactPickerActivity.java
@@ -6,7 +6,11 @@ import java.util.List;
 import sneerteam.android.main.Contact;
 import sneerteam.android.main.R;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 
 public class ContactPickerActivity extends Activity {
@@ -17,14 +21,18 @@ public class ContactPickerActivity extends Activity {
 		setContentView(R.layout.activity_main);
 
 		ListView listView = (ListView) findViewById(R.id.listView);
-		// listView.setOnItemClickListener(new OnItemClickListener() { @Override
-		// public void onItemClick(AdapterView<?> parent, View view, int
-		// position, long id) {
-		// String name = contacts().get(position).getName();
-		// Intent intent = new Intent(Contacts.this, ChatActivity.class);
-		// intent.putExtra("contact_seal", name);
-		// startActivity (intent);
-		// }});
+		listView.setOnItemClickListener(new OnItemClickListener() { @Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				Contact contact = contacts().get(position);
+				String name = contact.getName();
+				
+				Intent intent = new Intent();
+				intent.putExtra("public_key", name);
+				intent.putExtra("nickname", name);
+				
+				setResult(RESULT_OK, intent);
+				finish();
+		 }});
 
 		ContactsAdapter adapter = new ContactsAdapter(this, R.layout.list_item_contact, contacts());
 		listView.setAdapter(adapter);
