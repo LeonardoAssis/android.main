a4a03326389b8744ec270d005e1d0fca5359c990
Rodrigo B. de Oliveira <rbo@acm.org>
2014-03-22 00:06:01 -0300

    merge snapi with android.main

---
+++ src/sneerteam/android/main/CloudService.java
@@ -0,0 +1,26 @@
+package sneerteam.android.main;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+
+public class CloudService extends Service {
+	
+	private CloudMaster master;
+
+	@Override public void onCreate()  { super.onCreate(); master = new CloudMasterImpl(); };
+	@Override public void onDestroy() {	if (master != null) master.close();	super.onDestroy(); }
+	
+	@Override
+	public IBinder onBind(Intent intent) {
+		String caller = getPackageManager().getNameForUid(Binder.getCallingUid());
+		log("onBind(" + intent.getAction() + ") Caller: " + caller);
+		return master.freshCloudFor(caller);
+	}
+	
+	private void log(String message) {
+		Log.d(getClass().getCanonicalName(), message);
+	}
+}
